a26a4456a88e0f198126191fc732d7167f1b7f3b
futakuchi-eitaro <futakuchi-eitaro@dmm.com>
2025-05-25 20:58:43 +0900

    fix: Jira記法のMarkdownに対しても標準記法の定義を用いてattachInlineNodesToBlocks行っていた処理を修正
    InlineLexerとInlineParserのインポートを標準とJiraのそれぞれに明示的に変更しました。
    parseStandardMarkdownおよびparseJiraMarkdown関数内でそれぞれattachInlineNodesToBlocksを呼び出すようにしました。

--- src/parsers/markdown/index.ts
+++ src/parsers/markdown/index.ts
@@ -4,32 +4,35 @@ export type { MarkdownBlockNode };
 import { attachInlineNodesToBlocks } from "./common/ast/helpers/attachInlineNodesToBlocks";
 import { DomRendererVisitor } from "./common/ast/visitors/DomRendererVisitor";
 import { BlockLexer as JiraBlockLexer } from "./dialects/jira/lexer/BlockLexer";
+import { InlineLexer as JiraInlineLexer } from "./dialects/jira/lexer/InlineLexer";
 import { BlockParser as JiraBlockParser } from "./dialects/jira/parser/BlockParser";
+import { InlineParser as JiraInlineParser } from "./dialects/jira/parser/InlineParser";
 import { JiraMarkdownRendererVisitor } from "./dialects/jira/visitors/JiraMarkdownRendererVisitor";
 import { BlockLexer as StandardBlockLexer } from "./dialects/standard/lexer/BlockLexer";
-import { InlineLexer } from "./dialects/standard/lexer/InlineLexer";
+import { InlineLexer as StandardInlineLexer } from "./dialects/standard/lexer/InlineLexer";
 import { BlockParser as StandardBlockParser } from "./dialects/standard/parser/BlockParser";
-import { InlineParser } from "./dialects/standard/parser/InlineParser";
+import { InlineParser as StandardInlineParser } from "./dialects/standard/parser/InlineParser";
 import { StandardMarkdownRendererVisitor } from "./dialects/standard/visitors/StandardMarkdownRendererVisitor";
-
-function attachInline(ast: MarkdownBlockNode[]): MarkdownBlockNode[] {
-  return attachInlineNodesToBlocks(ast, (text) => {
-    const inlineTokens = new InlineLexer(text).tokenize();
-    return new InlineParser(inlineTokens).parse();
-  });
-}
 
 // === Parse ===
 export function parseStandardMarkdown(source: string): MarkdownBlockNode[] {
   const tokens = new StandardBlockLexer(source).tokenize();
   const ast = new StandardBlockParser(tokens).parse();
-  return attachInline(ast);
+
+  return attachInlineNodesToBlocks(ast, (text) => {
+    const inlineTokens = new StandardInlineLexer(text).tokenize();
+    return new StandardInlineParser(inlineTokens).parse();
+  });
 }
 
 export function parseJiraMarkdown(source: string): MarkdownBlockNode[] {
   const tokens = new JiraBlockLexer(source).tokenize();
   const ast = new JiraBlockParser(tokens).parse();
-  return attachInline(ast);
+
+  return attachInlineNodesToBlocks(ast, (text) => {
+    const inlineTokens = new JiraInlineLexer(text).tokenize();
+    return new JiraInlineParser(inlineTokens).parse();
+  });
 }
 
 // === Render ===
